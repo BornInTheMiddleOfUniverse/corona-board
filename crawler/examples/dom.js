dc645230929e0f9b8abb87ccee59916fdb3de286
youredith <110220308+BornInTheMiddleOfUniverse@users.noreply.github.com>
2022-09-26 21:42:12 +0900

    Crawler, Gatsby, Prettier settings

---
+++ crawler/examples/dom.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+async function main() {
+  //html로드
+  const resp = await axios.get(
+      'https://yjiq150.github.io/coronaboard-crawling-sample/dom'
+  );
+
+  const $ = cheerio.load(resp.data);
+  const elements = $('.slide p');
+
+  elements.each((idx, el) => {
+    console.log($(el).text());
+  });
+}
+
+main();
