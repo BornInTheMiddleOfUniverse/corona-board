b2252a222d09905563144c8052eeb056f1db327b
youredith <110220308+BornInTheMiddleOfUniverse@users.noreply.github.com>
2022-09-30 15:46:36 +0900

    React component
    - Basics of react component
    
    - CSS-in-JS with Emotion library(Supported by Gatsby plugin).

--- crawler/examples/dom.js
+++ crawler/examples/dom.js
@@ -2,17 +2,18 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 async function main() {
-  //html로드
-  const resp = await axios.get(
-      'https://yjiq150.github.io/coronaboard-crawling-sample/dom'
-  );
-
-  const $ = cheerio.load(resp.data);
-  const elements = $('.slide p');
-
-  elements.each((idx, el) => {
-    console.log($(el).text());
-  });
+    //html로드
+    const resp = await axios.get(
+        'https://yjiq150.github.io/coronaboard-crawling-sample/dom'
+    );
+    //
+    const $ = cheerio.load(resp.data);
+    const elements = $('.slide p');
+    //text() 메서드를 사용하기 위해 Node 객체인 el을 $로 감싸서 cheerio 객체로 변환
+    //el 변수에 담긴 요소는 cheerio에서 만들어낸 DOM상의 Node 객체인데 cheerio 객체로 감싸주면 cheerio에서 제공하는 기능 사용가능(ex. text())
+    elements.each((idx, el) => {
+        console.log($(el).text());
+    });
 }
 
 main();
